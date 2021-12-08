40ce6b6987a99e3e13bc97ecc004600e35ab0f64
Bruno Panizzi <brunopanizziq@gmail.com>
2021-12-08 18:30:18 -0300

    added cors middleware

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ const routes = require('./routes')
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json())  // body parser
+
+app.use((request, response, next) => {  // cors
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  response.setHeader('Access-Control-Allow-Methods', '*')
+  response.setHeader('Access-Control-Allow-Headers', '*')
+  next()
+})
+
 app.use(routes)
 
 
-app.listen(3000, () => console.log('App started at http://localhost:3000/'))
+app.listen(3001, () => console.log('App started at http://localhost:3001/'))
