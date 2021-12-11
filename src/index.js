73121d483b353e94702a283184886a30f9f2a604
Bruno Panizzi <brunopanizziq@gmail.com>
2021-12-11 15:19:28 -0300

    minor improvments

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ const app = express()
 app.use(express.json())  // body parser
 
 app.use((request, response, next) => {  // cors
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  response.setHeader('Access-Control-Allow-Origin', '*')
   response.setHeader('Access-Control-Allow-Methods', '*')
   response.setHeader('Access-Control-Allow-Headers', '*')
   response.setHeader('Access-Control-Max-Age', '60')
+  next()
+})
+
+app.use((request, response, next) => {
+  console.log(request.method, request.originalUrl)
   next()
 })
 
