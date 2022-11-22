17bb5151a0ae8ed527320828fbf874866bc4ef72
BrunoPanizzi <brunopanizziq@gmail.com>
2022-11-22 17:55:36 -0300

    added error handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
+require('express-async-errors')
 
 const routes = require('./routes')
 
 const app = express()
 
-app.use(express.json()) // body parser
+// body parser
+app.use(express.json())
 
+// cors
 app.use((request, response, next) => {
-  // cors
   response.setHeader('Access-Control-Allow-Origin', '*')
   response.setHeader('Access-Control-Allow-Methods', '*')
   response.setHeader('Access-Control-Allow-Headers', '*')
@@ -15,12 +17,20 @@ app.use((request, response, next) => {
   next()
 })
 
+// logging incoming requests
 app.use((request, response, next) => {
   console.log(request.method, request.originalUrl)
   next()
 })
 
+// routes
 app.use(routes)
+
+// error handler
+app.use((error, request, response, next) => {
+  console.log(error)
+  response.sendStatus(500)
+})
 
 app.listen(3001, () =>
   console.log('App started at http://localhost:3001/')
